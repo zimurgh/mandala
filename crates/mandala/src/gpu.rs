46227ee0b46db5eb833a8ebfbf3ddcb1640acfff
Michael Carpenter <mcarpenter.dev@gmail.com>
2024-07-16 22:35:56 -0400

    Remove validation layer from test

--- crates/mandala/src/gpu.rs
+++ crates/mandala/src/gpu.rs
@@ -41,7 +41,6 @@ impl Gpu {
 #[cfg(test)]
 mod tests {
     use core::ffi;
-    use std::ffi::c_char;
 
     use ash::{ext::debug_utils, vk};
 
@@ -53,16 +52,6 @@ mod tests {
 
         let app_name = unsafe { ffi::CStr::from_bytes_with_nul_unchecked(b"Mandala\0") };
 
-        let layer_names = unsafe {
-            [ffi::CStr::from_bytes_with_nul_unchecked(
-                b"VK_LAYER_KHRONOS_validation\0",
-            )]
-        };
-
-        let layers_names_raw: Vec<*const c_char> = layer_names
-            .iter()
-            .map(|raw_name| raw_name.as_ptr())
-            .collect();
         let extension_names = vec![debug_utils::NAME.as_ptr()];
 
         let application_info = vk::ApplicationInfo::default()
@@ -72,7 +61,6 @@ mod tests {
             .engine_version(0)
             .api_version(vk::make_api_version(0, 1, 3, 0));
         let create_info = vk::InstanceCreateInfo::default()
-            .enabled_layer_names(&layers_names_raw)
             .enabled_extension_names(&extension_names)
             .application_info(&application_info);
         let instance = unsafe { entry.create_instance(&create_info, None).unwrap() };

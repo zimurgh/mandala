42cd6e15ef52b9ee3b16f1c88eaae7addfcf119f
Michael Carpenter <mcarpenter.dev@gmail.com>
2024-07-16 22:53:01 -0400

    Remove vulkan testing for now

--- crates/mandala/src/gpu.rs
+++ crates/mandala/src/gpu.rs
@@ -1,6 +1,9 @@
-use std::sync::Arc;
+use std::{
+    ffi::{self, c_char},
+    sync::Arc,
+};
 
-use ash::{vk, Entry};
+use ash::{ext::debug_utils, vk, Entry};
 use winit::{event_loop::ActiveEventLoop, window::Window};
 
 use crate::error::GpuResult;
@@ -20,6 +23,18 @@ impl Gpu {
 
         let entry = unsafe { Entry::load()? };
 
+        let layer_names = unsafe {
+            [ffi::CStr::from_bytes_with_nul_unchecked(
+                b"VK_LAYER_KHRONOS_validation\0",
+            )]
+        };
+
+        let layers_names_raw: Vec<*const c_char> = layer_names
+            .iter()
+            .map(|raw_name| raw_name.as_ptr())
+            .collect();
+        let extension_names = vec![debug_utils::NAME.as_ptr()];
+
         let app_name = unsafe { std::ffi::CStr::from_bytes_with_nul_unchecked(b"Mandala\0") };
         let application_info = vk::ApplicationInfo::default()
             .application_name(app_name)
@@ -27,7 +42,10 @@ impl Gpu {
             .engine_name(app_name)
             .engine_version(0)
             .api_version(vk::make_api_version(0, 1, 3, 0));
-        let create_info = vk::InstanceCreateInfo::default().application_info(&application_info);
+        let create_info = vk::InstanceCreateInfo::default()
+            .enabled_layer_names(&layers_names_raw)
+            .enabled_extension_names(&extension_names)
+            .application_info(&application_info);
         let instance = unsafe { entry.create_instance(&create_info, None).unwrap() };
         let physical_devices = unsafe { instance.enumerate_physical_devices().unwrap() };
         assert!(!physical_devices.is_empty());
@@ -35,30 +53,5 @@ impl Gpu {
         Ok(Gpu {
             window: Some(window),
         })
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use core::ffi;
-
-    use super::*;
-
-    #[test]
-    fn test_gpu_init() {
-        let entry = Entry::linked();
-
-        let app_name = unsafe { ffi::CStr::from_bytes_with_nul_unchecked(b"Mandala\0") };
-
-        let application_info = vk::ApplicationInfo::default()
-            .application_name(app_name)
-            .application_version(0)
-            .engine_name(app_name)
-            .engine_version(0)
-            .api_version(vk::make_api_version(0, 1, 3, 0));
-        let create_info = vk::InstanceCreateInfo::default().application_info(&application_info);
-        let instance = unsafe { entry.create_instance(&create_info, None).unwrap() };
-        let physical_devices = unsafe { instance.enumerate_physical_devices().unwrap() };
-        assert!(!physical_devices.is_empty());
     }
 }

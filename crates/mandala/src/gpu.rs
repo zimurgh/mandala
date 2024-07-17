c41f278e5a05eeab7aad416eda975c56504f7793
Michael Carpenter <mcarpenter.dev@gmail.com>
2024-07-16 22:38:05 -0400

    Remove extensions from test

--- crates/mandala/src/gpu.rs
+++ crates/mandala/src/gpu.rs
@@ -42,8 +42,6 @@ impl Gpu {
 mod tests {
     use core::ffi;
 
-    use ash::{ext::debug_utils, vk};
-
     use super::*;
 
     #[test]
@@ -52,17 +50,13 @@ mod tests {
 
         let app_name = unsafe { ffi::CStr::from_bytes_with_nul_unchecked(b"Mandala\0") };
 
-        let extension_names = vec![debug_utils::NAME.as_ptr()];
-
         let application_info = vk::ApplicationInfo::default()
             .application_name(app_name)
             .application_version(0)
             .engine_name(app_name)
             .engine_version(0)
             .api_version(vk::make_api_version(0, 1, 3, 0));
-        let create_info = vk::InstanceCreateInfo::default()
-            .enabled_extension_names(&extension_names)
-            .application_info(&application_info);
+        let create_info = vk::InstanceCreateInfo::default().application_info(&application_info);
         let instance = unsafe { entry.create_instance(&create_info, None).unwrap() };
         let physical_devices = unsafe { instance.enumerate_physical_devices().unwrap() };
         assert!(!physical_devices.is_empty());
